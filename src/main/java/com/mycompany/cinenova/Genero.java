fd34bfd9ccc9889e0c8261e34c82af47c0ca5593
Apuy2007 <derekapuy2134@gmail.com>
2026-06-29 13:56:38 -0600

    Update Genero.java

--- src/main/java/com/mycompany/cinenova/Genero.java
+++ src/main/java/com/mycompany/cinenova/Genero.java
@@ -7,5 +7,5 @@ package com.mycompany.cinenova;
  * Este es el enum donde se establecen los valores para el genero de la pelicula.
  */
 public enum Genero {
-    Noasignado,Accion,Comedia,Drama,Terror,ciencia_Ficcion,Animacion,Aventura
+    Accion,Comedia,Drama,Terror,ciencia_Ficcion,Animacion,Aventura
 }
